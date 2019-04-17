b0df76e035a828803a0cb4a8f5d74bca22da8562
Qiu <1044375652@qq.com>
2019-04-17 14:06:00 +0800

    add change ip

--- app/src/main/java/com/example/administrator/tongxianghui/src/four/ChooseBusMessageActivity.java
+++ app/src/main/java/com/example/administrator/tongxianghui/src/four/ChooseBusMessageActivity.java
@@ -1,17 +1,57 @@
 package com.example.administrator.tongxianghui.src.four;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.administrator.tongxianghui.R;
+import com.example.administrator.tongxianghui.dao.DataBaseHelper;
+import com.example.administrator.tongxianghui.model.PointMessagesInfo;
+import com.example.administrator.tongxianghui.model.base.Res;
+import com.example.administrator.tongxianghui.utils.Ip;
+import com.example.administrator.tongxianghui.utils.PointMessage;
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChooseBusMessageActivity extends AppCompatActivity {
 
     private Bundle bundle;
     private static final String TAG = ChooseBusMessageActivity.class.getName();
     private Context context;
+    private int directionType;
+    private TextView fourCurrentDirection;
+    private DataBaseHelper dataBaseHelper;
+    private List<PointMessagesInfo> pointMessagesInfoList;
+    private OkHttpClient okHttpClient;
+    private RequestBody requestBody;
+    private Request request;
+    private Call call;
+    private Response response;
+    private Gson gson;
+    private List<String> upPointList;
+    private List<String> downPointList;
+    private Handler handler;
+    private static final String Get_Point_Messages_Url = "http://" + Ip.IP + ":8001/point/messages";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +59,123 @@ public class ChooseBusMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choose_bus_message);
         context = ChooseBusMessageActivity.this;
         bundle = getIntent().getBundleExtra("directionType");
-        int directionType = bundle.getInt("directionType");
-        toast(directionType + "");
+        directionType = bundle.getInt("directionType");
+        dataBaseHelper = DataBaseHelper.getDataBaseHelper(context);
+        handler = new Handler();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fourCurrentDirection = findViewById(R.id.fourCurrentDirection);
+        fourCurrentDirection.setText(switchDirectionMsg(directionType));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String[] columns = new String[]{"_id", "name", "point_type", "direction_type", "point_status"};
+        pointMessagesInfoList = dataBaseHelper.selectDataFromPointMessageTable(columns, null, null);
+        if (pointMessagesInfoList.size() == 0) {
+
+        } else {
+            if (pointMessagesInfoList.get(0).getPointStatus() == 1) {
+
+            }
+        }
     }
 
     private void toast(String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 
+
+    private String switchDirectionMsg(int directionType) {
+        String msg = "当前乘车方向：";
+        switch (directionType) {
+            case 0:
+                msg += "惠城、博罗至珠海同乡会包车";
+                break;
+            case 1:
+                msg += "珠海至惠城、博罗同乡会包车";
+                break;
+            case 2:
+                msg += "珠海至惠东、淡水客运站班次";
+                break;
+            case 3:
+                msg += "惠东、淡水至珠海客运站班次";
+                break;
+            case 4:
+                msg += "惠东、淡水至珠海同乡会包车";
+                break;
+            case 5:
+                msg += "珠海至惠东、淡水同乡会包车";
+                break;
+        }
+        return msg;
+    }
+
+    private List<PointMessage> requestDataFromService() {
+        okHttpClient = new OkHttpClient();
+        request = new Request.Builder()
+                .url(Get_Point_Messages_Url)
+                .build();
+        call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i(TAG, "请求服务器失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                gson = new Gson();
+                Res res = gson.fromJson(response.body().string(), Res.class);
+                if (res.getCode() == 200) {
+                    PointMessagesInfo[] pointMessagesInfos = gson.fromJson(String.valueOf(res.getData()), PointMessagesInfo[].class);
+                    pointMessagesInfoList = Arrays.asList(pointMessagesInfos);
+                    dataBaseHelper.addDataToPointMessageTable(pointMessagesInfoList);
+                }
+            }
+        });
+        return null;
+    }
+
+    private void showUpAndDownPointView(List<PointMessagesInfo> pointMessagesInfoList) {
+
+        upPointList = new ArrayList<>();
+        downPointList = new ArrayList<>();
+        for (PointMessagesInfo pointMessagesInfo : pointMessagesInfoList) {
+            switch (pointMessagesInfo.getPointType()) {
+                case 0:
+                    upPointList.add(pointMessagesInfo.getName());
+                    break;
+                case 1:
+                    downPointList.add(pointMessagesInfo.getName());
+                    break;
+            }
+        }
+
+        ListView upPointListView = new ListView(context);
+        ListView downPointListView = new ListView(context);
+
+        ArrayAdapter upPointArrayAdapter = new ArrayAdapter(context, R.layout.choose_point_item, upPointList);
+        ArrayAdapter downPointArrayAdapter = new ArrayAdapter(context, R.layout.choose_point_item, downPointList);
+
+        upPointListView.setAdapter(upPointArrayAdapter);
+        downPointListView.setAdapter(downPointArrayAdapter);
+
+        LinearLayout upPointLinearLayout = findViewById(R.id.fourUpPointGroup);
+        LinearLayout downPointLinearLayout = findViewById(R.id.fourDownPointGroup);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                upPointLinearLayout.addView(upPointListView);
+                downPointLinearLayout.addView(downPointListView);
+            }
+        });
+    }
 
 }
